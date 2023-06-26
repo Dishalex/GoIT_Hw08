7dd4f5d3655468875d63b2217f87acc4fa316e9a
Dishalex <131618968+Dishalex@users.noreply.github.com>
2023-06-26 17:48:29 +0300

    Update Hw08.py

--- Hw08.py
+++ Hw08.py
@@ -1,40 +1,38 @@
 from datetime import datetime, timedelta
 from collections import defaultdict
 
+employees = [{"name": "Angel", "birthdate": "6.07.2000"},
+             {"name": "Mike", "birthdate": datetime(1985, 6, 30)},
+             {"name": "Sandra", "birthdate": datetime(1989, 7, 7)},
+             {"name": "Nik", "birthdate": datetime(1913, 7, 1)},
+             {"name": "Piter", "birthdate": datetime(1990, 7, 2)},
+             {"name": "Kurka", "birthdate": datetime(1970, 7, 5)}]
 
-employees = [{"name": "Pitier", "birthdate": datetime(1990, 6, 28)},
-             {"name": "Angel", "birthdate": "24.06.2000"},
-             {"name": "Angel", "birthdate": datetime(1985, 6, 28)}]
-
-
-def get_period() -> tuple[datetime.date, datetime.date]:
+def get_period() -> tuple[int, int]:
     current_date = datetime.now()
     start_period = current_date + timedelta(days=5-current_date.weekday())
     return start_period.date(), (start_period + timedelta(6)).date()
 
+start, end = get_period() # Отримання періоду винесено за межі циклу, щоб не обчислювати багато разів одне і те ж
 
 def check_epl(list_of_emp: list) -> None:
     result = defaultdict(list)
     current_year = datetime.now().year
     for employee in list_of_emp:
         bd = employee["birthdate"]
-        if isinstance(bd, datetime):
-            bd = bd.date()
-        else:
-            bd = datetime.strptime(bd, "%d.%m.%Y").date()
-        bd = bd.replace(year=current_year)
-
-        start, end = get_period()
-
+        if not isinstance(bd, datetime):
+            bd = datetime.strptime(bd, "%d.%m.%Y")
+        bd = bd.date().replace(year=current_year)
+        
         if start <= bd <= end:
             if bd.weekday() in (5, 6):
-                result[bd].append(employee["name"])
+                result[bd + timedelta(days=(7-bd.weekday()))].append(employee["name"])
             else:
                 result[bd].append(employee["name"])
     return result
 
-
 if __name__ == "__main__":
-    for key, value in check_epl(employees).items():
-        print(key.strftime("%A"), value)
-    # print(get_period())
+    
+    for key, value in sorted(check_epl(employees).items()):
+        print(f'{key.strftime("%A")}', end = ': ')
+        print(*value, sep=', ')
